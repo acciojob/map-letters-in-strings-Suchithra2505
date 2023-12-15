bca270b55802db783408c338804ed26da6a436d7
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-15 19:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 function mapletters(str){
 	let ans={};
 	for(let i=0;i<str.length;i++){
-		//const l = str[i];
-		if(ans["str[i]")
-			ans["str[i]"].push(i);
+		const l = str[i];
+		if(ans["l"])
+			ans[l].push(i);
 		else
-			ans["str[i]"]=[i];
+			ans[l]=[i];
 	}
-	console.log(ans);
+	//console.log(ans);
+	return ans;
 }
 
-const text = prompt("Enter text:");
-mapletters(text);
+//const text = prompt("Enter text:");
+//mapletters(text);
+console.log(mapLetters("dodo"));
