04fd87efe656faf02ac62d586b22a524da92342d
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-15 19:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapletters(str){
+	let ans={};
+	for(let i=0:i<str.length;i++){
+		const l = str[i];
+		if(ans["l")
+			ans[l].push(i);
+		else
+			ans[l]=[i]
+	}
+	return ans
+}
+
+const text = prompt("Enter text:");
+alert(mapletters(text));
